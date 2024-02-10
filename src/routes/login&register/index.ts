5a22945408bb46af78dadab3d56adf2eda80b80c
SolucoGabriel <gabrielfagundessilveira@gmail.com>
2024-02-10 12:40:21 -0300

    [FIX]: middleware moved from BackEnd to FrontEnd

--- src/routes/login&register/index.ts
+++ src/routes/login&register/index.ts
@@ -5,31 +5,6 @@ import bcrypt from 'bcrypt';
 import 'dotenv/config';
 
 const router = Router();
-const SECRET: string = process.env.SECRET as string;
-
-const loggedMiddleware: RequestHandler = (req, res, next) => {
-    const auth = req.headers.authorization || '';
-
-    const parts = auth.split(' ');
-
-    if(parts.length != 2)
-        return res.status(401).send();
-
-    const [prefix, token] = parts;
-
-    if(prefix !== 'Bearer')
-        return res.status(401).send();
-
-    jwt.verify(token, SECRET, (error, decoded) => {
-        if(error) {
-            return res.status(401).send(error);
-        }
-
-        res.locals.creatorId = (decoded as jwt.JwtPayload).id;
-
-        next();
-    });
-};
 
 // user register
 router.post('/register', async (req, res) => {
@@ -64,22 +39,17 @@ router.post('/register', async (req, res) => {
 
     const users = await prisma.user.findMany({ });
 
-    const token = jwt.sign(newUser, SECRET, {
-        expiresIn: '5h'
-    });
-
     if(role === "Admin") {
-        return res.status(200).send({ users, token });
+        return res.status(200).send({ users });
     } else if (role === "User") {
-        return res.status(200).send({ name, token });
+        return res.status(200).send({ name });
     } else (!role); {
         return res.status(401).send('undefined role');
     }
-
 });
 
 // user login
-router.post('/login', loggedMiddleware, async (req, res) => {
+router.post('/login', async (req, res) => {
     const {email, password} = req.body;
     const infoNeeded = email && password;
 
@@ -119,37 +89,11 @@ router.post('/login', loggedMiddleware, async (req, res) => {
 });
 
 // users info
-router.get('/users_info', loggedMiddleware, async (req, res) => {
-    const { email, password } = req.body;
-    const infoNeeded = email && password;
-
-    if (!infoNeeded) {
-        return res.status(401).send('missing fields');
-    }
+router.get('/admin', async (_, res) => {
 
     const users = await prisma.user.findMany({ });
 
-    const user = await prisma.user.findFirst({
-        where: {
-            email
-        },
-        select: {
-            id: true,
-            name: true, 
-            password: true,
-            role: true
-        }
-    });
-
-    if (!user) {
-        return res.status(404).send('please be registered');
-    }
-
-    if (user.role == 'Admin') {
-        return res.status(200).send({ users });
-    } else (!user.role); {
-        return res.status(401).send('undefined role')
-    }
+    return res.status(200).send({ users })
 });
 
 export default router;
